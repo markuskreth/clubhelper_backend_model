dc9f35a13143dfeb0e81f00e14a87ca324a12d74
Markus Kreth <markus.kreth@web.de>
2020-10-24 18:44:11 +0200

    try to activate swagger - not working yet.

--- src/main/java/de/kreth/clubhelper/model/ClubhelperModelApplication.java
+++ src/main/java/de/kreth/clubhelper/model/ClubhelperModelApplication.java
@@ -3,11 +3,14 @@ package de.kreth.clubhelper.model;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class ClubhelperModelApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClubhelperModelApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(ClubhelperModelApplication.class, args);
+    }
 
 }
