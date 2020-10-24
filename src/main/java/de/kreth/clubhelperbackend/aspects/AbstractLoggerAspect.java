60463561736a27451ad0f4c2f68880cceda792c8
Markus Kreth <markus.kreth@web.de>
2020-10-24 18:37:15 +0200

    Logger Aspect logging with Logged class name

--- src/main/java/de/kreth/clubhelperbackend/aspects/AbstractLoggerAspect.java
+++ src/main/java/de/kreth/clubhelperbackend/aspects/AbstractLoggerAspect.java
@@ -1,99 +1,115 @@
 package de.kreth.clubhelperbackend.aspects;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.aspectj.lang.JoinPoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AbstractLoggerAspect {
 
-	protected Logger logger = LoggerFactory.getLogger(getClass());
+    private final Map<Class<?>, Logger> loggerCache = new HashMap<>();
 
-	public enum LogLevel {
-		WARN, DEBUG, INFO;
+    public enum LogLevel {
+	WARN, DEBUG, INFO;
 
-		boolean enabled(Logger logger) {
-			switch (this) {
-				case WARN :
-					return logger.isWarnEnabled();
-				case DEBUG :
-					return logger.isDebugEnabled();
-				case INFO :
-					return logger.isInfoEnabled();
+	boolean enabled(Logger logger) {
+	    switch (this) {
+	    case WARN:
+		return logger.isWarnEnabled();
+	    case DEBUG:
+		return logger.isDebugEnabled();
+	    case INFO:
+		return logger.isInfoEnabled();
 
-				default :
-					return true;
-			}
+	    default:
+		return true;
+	    }
+	}
+    }
+
+    protected void log(LogLevel level, JoinPoint joinPoint) {
+	log(level, joinPoint, null);
+    }
+
+    protected void log(LogLevel level, JoinPoint joinPoint, Object result) {
+
+	Logger logger = getLoggerFor(joinPoint);
+	if (level.enabled(logger)) {
+
+	    StringBuffer msg = generateLogMessage(joinPoint);
+	    Exception exception = null;
+
+	    if (result != null) {
+		if (result instanceof Exception) {
+		    exception = (Exception) result;
+		} else {
+		    msg.append(" ==> ").append(result);
 		}
+	    }
+
+	    switch (level) {
+	    case WARN:
+		if (exception == null) {
+		    logger.warn(msg.toString());
+		} else {
+		    logger.warn(msg.toString(), exception);
+		}
+		break;
+	    case DEBUG:
+		if (exception == null) {
+		    logger.debug(msg.toString());
+		} else {
+		    logger.debug(msg.toString(), exception);
+		}
+		break;
+	    case INFO:
+		if (exception == null) {
+		    logger.info(msg.toString());
+		} else {
+		    logger.info(msg.toString(), exception);
+		}
+		break;
+	    default:
+		if (exception == null) {
+		    logger.trace(msg.toString());
+		} else {
+		    logger.trace(msg.toString(), exception);
+		}
+	    }
+	}
+    }
+
+    protected StringBuffer generateLogMessage(JoinPoint joinPoint) {
+	StringBuffer logMessage = new StringBuffer();
+	logMessage.append(joinPoint.getTarget().getClass().getName());
+	logMessage.append(".");
+	logMessage.append(joinPoint.getSignature().getName());
+	logMessage.append("(");
+	// append args
+	Object[] args = joinPoint.getArgs();
+	for (int i = 0; i < args.length; i++) {
+	    logMessage.append(args[i]).append(",");
+	}
+	if (args.length > 0) {
+	    logMessage.deleteCharAt(logMessage.length() - 1);
 	}
 
-	protected void log(LogLevel level, JoinPoint joinPoint) {
-		log(level, joinPoint, null);
+	logMessage.append(")");
+	return logMessage;
+    }
+
+    protected Logger getLoggerFor(JoinPoint joinPoint) {
+	Class<?> logeventClass = joinPoint.getTarget().getClass();
+	Logger logger;
+	if (loggerCache.containsKey(logeventClass)) {
+	    logger = loggerCache.get(logeventClass);
+	} else {
+	    logger = LoggerFactory.getLogger(logeventClass);
+	    loggerCache.put(logeventClass, logger);
 	}
-
-	protected void log(LogLevel level, JoinPoint joinPoint, Object result) {
-
-		if (level.enabled(logger)) {
-
-			StringBuffer msg = generateLogMessage(joinPoint);
-			Exception exception = null;
-
-			if (result != null) {
-				if (result instanceof Exception) {
-					exception = (Exception) result;
-				} else {
-					msg.append(" ==> ").append(result);
-				}
-			}
-
-			switch (level) {
-				case WARN :
-					if (exception == null) {
-						logger.warn(msg.toString());
-					} else {
-						logger.warn(msg.toString(), exception);
-					}
-					break;
-				case DEBUG :
-					if (exception == null) {
-						logger.debug(msg.toString());
-					} else {
-						logger.debug(msg.toString(), exception);
-					}
-					break;
-				case INFO :
-					if (exception == null) {
-						logger.info(msg.toString());
-					} else {
-						logger.info(msg.toString(), exception);
-					}
-					break;
-				default :
-					if (exception == null) {
-						logger.trace(msg.toString());
-					} else {
-						logger.trace(msg.toString(), exception);
-					}
-			}
-		}
-	}
-
-	protected StringBuffer generateLogMessage(JoinPoint joinPoint) {
-		StringBuffer logMessage = new StringBuffer();
-		logMessage.append(joinPoint.getTarget().getClass().getName());
-		logMessage.append(".");
-		logMessage.append(joinPoint.getSignature().getName());
-		logMessage.append("(");
-		// append args
-		Object[] args = joinPoint.getArgs();
-		for (int i = 0; i < args.length; i++) {
-			logMessage.append(args[i]).append(",");
-		}
-		if (args.length > 0) {
-			logMessage.deleteCharAt(logMessage.length() - 1);
-		}
-
-		logMessage.append(")");
-		return logMessage;
-	}
+	return logger;
+    }
 
 }
--- src/main/java/de/kreth/clubhelperbackend/aspects/ControllerLoggerAspect.java
+++ src/main/java/de/kreth/clubhelperbackend/aspects/ControllerLoggerAspect.java
@@ -16,41 +16,41 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class ControllerLoggerAspect extends AbstractLoggerAspect {
 
-	@Pointcut("execution (public * de.kreth.clubhelperbackend.controller..*.delete(..))")
-	private void deleteItem() {
-	}
+    @Pointcut("execution (public * de.kreth.clubhelperbackend.controller..*.delete(..))")
+    private void deleteItem() {
+    }
 
-	@Pointcut("execution (public * de.kreth.clubhelperbackend.controller..*(..))")
-	private void callAny() {
-	}
+    @Pointcut("execution (public * de.kreth.clubhelperbackend.controller..*(..))")
+    private void callAny() {
+    }
 
-	@Pointcut("callAny() && (!deleteItem())")
-	private void invocation() {
-	}
+    @Pointcut("callAny() && (!deleteItem())")
+    private void invocation() {
+    }
 
-	@Before("invocation()")
-	public void logDao(JoinPoint joinPoint) throws Throwable {
-		log(INFO, joinPoint);
-	}
+    @Before("invocation()")
+    public void logDao(JoinPoint joinPoint) throws Throwable {
+	log(INFO, joinPoint);
+    }
 
-	@AfterThrowing(pointcut = "invocation()", throwing = "ex")
-	public void logCall(JoinPoint joinPoint, Exception ex) throws Throwable {
-		logger.error(generateLogMessage(joinPoint).toString(), ex);
-	}
+    @AfterThrowing(pointcut = "invocation()", throwing = "ex")
+    public void logCall(JoinPoint joinPoint, Exception ex) throws Throwable {
+	getLoggerFor(joinPoint).error(generateLogMessage(joinPoint).toString(), ex);
+    }
 
-	@AfterReturning(pointcut = "invocation()", returning = "result")
-	public void logCall(JoinPoint joinPoint, Object result) throws Throwable {
-		log(DEBUG, joinPoint, result);
-	}
+    @AfterReturning(pointcut = "invocation()", returning = "result")
+    public void logCall(JoinPoint joinPoint, Object result) throws Throwable {
+	log(DEBUG, joinPoint, result);
+    }
 
-	@AfterReturning(pointcut = "deleteItem()", returning = "result")
-	public void logDeleteSuccess(JoinPoint joinPoint, Object result)
-			throws Throwable {
-		log(WARN, joinPoint, result);
-	}
+    @AfterReturning(pointcut = "deleteItem()", returning = "result")
+    public void logDeleteSuccess(JoinPoint joinPoint, Object result)
+	    throws Throwable {
+	log(WARN, joinPoint, result);
+    }
 
-	@Before("deleteItem()")
-	public void logDeleteInvocation(JoinPoint joinPoint) throws Throwable {
-		log(DEBUG, joinPoint);
-	}
+    @Before("deleteItem()")
+    public void logDeleteInvocation(JoinPoint joinPoint) throws Throwable {
+	log(DEBUG, joinPoint);
+    }
 }
